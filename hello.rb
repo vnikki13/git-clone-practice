f881d62f31c5034b3ad25d8273bc2bf54f216467
Nikki Vaughan <vnikki13@gmail.com>
2020-02-10 11:07:11 -0800

    added a greeting feature

--- hello.rb
+++ hello.rb
@@ -3,3 +3,13 @@ puts "Hello World!"
 
 # This is a cool project
 puts "This is a cool project"
+
+# greeting message
+def greet(friend)
+  puts "Hello #{friend},"
+  puts "I'm learning so much at ada!"
+end
+
+puts "Enter the name of who you would like to greet"
+print "=> "
+greet(gets.chomp)
